ed537c461f26335adc5a0b6d28ac52b4a602cfd0
niculae niculae <niculaenick@gmail.com>
2024-03-01 20:43:34 +0100

    update sdk 1.5

---
+++ Contacts.Tests/HelpViewModelTest.cs
@@ -0,0 +1,15 @@
+﻿using Contacts.ViewModels;
+
+namespace Contacts.Tests;
+[TestClass]
+internal class HelpViewModelTest
+{
+
+    [TestMethod]
+    public void ShouldBeCreated()
+    {
+        var helpViewModel = new HelpViewModel();
+
+        Assert.IsNotNull(helpViewModel);
+    }
+}
